a5e4af5a772d0d6ccf2a1d7384f14d6db5642082
Grant Chinn <grant.chinn@gmail.com>
2016-02-11 21:15:51 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,13 +26,66 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+
+	console.log("User clicked on project" + idNumber);
+	$.get("/project/" + idNumber, addProject);
+	console.log("/project/" + idNumber);
 }
+
+function addProject(result){
+
+	console.log(result);
+	var projectHTML = '<a href="#" class ="thumbnail">' + 
+	'<img src ="' +result['image']+ '"class="detailsImage">' +
+	 '<p>' + result['title'] + '</p>' +
+	 '<p><small>' + result['date'] + '</small></p></a>';
+
+	 var divID = "#project" + result.id;
+
+	 console.log(projectHTML);
+
+	
+
+	 $(divID+ " .details").html(projectHTML + result['summary']);
+	 
+}
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
+	e.preventDefault();
+
+	//var paletteID = $(this).closest('.palettes').attr('id');
+
+
 	console.log("User clicked on color button");
+
+	$.get("/palette/", addColor);
+
+	console.log("/palette/");
+
 }
+
+
+function addColor(result){
+	console.log(result);
+
+	var colors = result['colors'].hex; 
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
+
+
+}
+
+
+
+
+
